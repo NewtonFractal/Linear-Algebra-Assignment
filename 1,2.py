6176b24b21a65f21dbd46157cbb0066a87945ac5
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-09-19 16:01:07 +0200

    Update 1,2.py

--- 1,2.py
+++ 1,2.py
@@ -1,16 +1,11 @@
 import numpy as np
 from scipy.optimize import fmin
-x1 = 0
-x2 = 0
-x3 = 0
 
-parameters = np.array([x1, x2, x3])
+parameters = np.array([0, 0, 0])
 goal = np.array([1,-1,1,-1])
 
 def optimize(array):
-    x1 = array[0]
-    x2 = array[1]
-    x3 = array[2]
+    x1,x2,x3 = array[0],array[1],array[2]
     x = [x1,x2,x3]
     guess = np.array([[1,1,2],
                  [1,2,1],
@@ -19,8 +14,6 @@ def optimize(array):
     objective = np.abs((guess@x)-goal)
     norm = np.linalg.norm(objective)
     return norm
-
-
 
 optimization = fmin(optimize,parameters,xtol=0.001)
 
