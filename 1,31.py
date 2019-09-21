5c067c3f1f93c2dd4a1a02326e1ec6adf048747d
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-09-21 16:43:03 +0200

    Create 1,31.py

---
+++ 1,31.py
@@ -0,0 +1,25 @@
+import numpy
+import matplotlib.pyplot as plt
+norm_list_y = []
+norm_list_x = []
+A = numpy.array([[1,1,2],
+                 [1,2,1],
+                 [2,1,1],
+                 [2,2,1]])
+
+A_Transpose = numpy.array([[1,1,2,2],
+                           [1,2,1,2],
+                           [2,1,1,1]])
+
+for a in range(1,1000,20):
+    a = a/100
+    norm_list_x.append(a)
+    y = numpy.array([1, a, 1, a])
+    numpy.matmul(A_Transpose, y)
+    inverse_matrix = numpy.linalg.inv(numpy.matmul(A_Transpose, A))
+    norm_list_y.append(numpy.linalg.norm(numpy.matmul(inverse_matrix, numpy.matmul(A_Transpose, y))))
+
+plt.plot(norm_list_x,norm_list_y)
+plt.ylabel('norm')
+plt.xlabel('parameter value')
+plt.show()
