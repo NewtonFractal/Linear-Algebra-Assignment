085fa612b9ec2ee22f567998b5feb0b3ed802308
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-10-10 14:48:34 +0200

    Update 2.1.2.py

--- 2.1.2.py
+++ 2.1.2.py
@@ -13,9 +13,7 @@ def recursive():
     q_n = np.matmul(np.matmul(vector, A), vector)
     print(q_n)
 
-
 recursive()
-
 
 def recursive_2():
     a_n_1, a_n, b_n_1, b_n, c_n_1, c_n = 8, 41, 3, 24, 12, 37
@@ -37,20 +35,7 @@ def recursive_2():
             print(x)
             print(diff)
             break
-
-
-recursive_2()
-
-def recursive_3():
-    a_n_1, a_n, b_n_1, b_n, c_n_1, c_n = 8, 41, 3, 24, 12, 37
-    for x in range(0, 500):
-        a_n_1, a_n, b_n_1, b_n, c_n_1, c_n = a_n, a_n_1 + 3 * b_n_1 + 2 * c_n_1, b_n, -3 * a_n_1 + 4 * b_n_1 + 3 * c_n_1, c_n, 2 * a_n_1 + 3 * b_n_1 + c_n_1
-    vector = np.array([a_n, b_n, c_n])
-    normed = ((a_n ** 2) + (b_n ** 2) + (c_n ** 2)) ** (1 / 2)
-    limit = vector / normed
-    print(limit)
-    a_n_1, a_n, b_n_1, b_n, c_n_1, c_n = 8, 41, 3, 24, 12, 37
-    for x in range(0, 500):
+    for x in range(x+1,500):
         a_n_1, a_n, b_n_1, b_n, c_n_1, c_n = a_n, a_n_1 + 3 * b_n_1 + 2 * c_n_1, b_n, -3 * a_n_1 + 4 * b_n_1 + 3 * c_n_1, c_n, 2 * a_n_1 + 3 * b_n_1 + c_n_1
         vector = np.array([a_n, b_n, c_n])
         normed = ((a_n ** 2) + (b_n ** 2) + (c_n ** 2)) ** (1 / 2)
@@ -58,8 +43,9 @@ def recursive_3():
         epsilon = 10 ** (-16)
         diff = np.linalg.norm(limit - limit_2)
         if diff < epsilon:
-            print(diff)
             print(x)
-            break
+            print(diff)
+            return None
 
-recursive_3()
+
+recursive_2()
