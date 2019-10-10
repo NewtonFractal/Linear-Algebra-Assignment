6d8acf84718b6ac6adac4a0651c72be6d6a82146
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-10-10 16:11:20 +0200

    Update 2.1.2.py

--- 2.1.2.py
+++ 2.1.2.py
@@ -3,21 +3,52 @@ import matplotlib.pyplot as plot
 
 x_values = []
 epsilon_values = []
+x_values_2 =[]
+epsilon_values_2 = []
 
 A = np.array([[1, 3, 2],
               [-3, 4, 3],
               [2, 3, 1]])
 
-
 def recursive():
     a_n_1, a_n, b_n_1, b_n, c_n_1, c_n = 8, 41, 3, 24, 12, 37
-    for x in range(0, 40):
+    for x in range(0, 500):
         a_n_1, a_n, b_n_1, b_n, c_n_1, c_n = a_n, a_n_1 + 3 * b_n_1 + 2 * c_n_1, b_n, -3 * a_n_1 + 4 * b_n_1 + 3 * c_n_1, c_n, 2 * a_n_1 + 3 * b_n_1 + c_n_1
-    vector = [a_n, b_n, c_n]
-    q_n = np.matmul(np.matmul(vector, A), vector)
+        vector = np.array([a_n, b_n, c_n])
+        normed = ((a_n ** 2) + (b_n ** 2) + (c_n ** 2)) ** (1 / 2)
+        limit = vector / normed
+        q_n = np.matmul(np.matmul(limit, A),limit)
     print(q_n)
+    a_n_1, a_n, b_n_1, b_n, c_n_1, c_n = 8, 41, 3, 24, 12, 37
+    for x in range(1, 500):
+        a_n_1, a_n, b_n_1, b_n, c_n_1, c_n = a_n, a_n_1 + 3 * b_n_1 + 2 * c_n_1, b_n, -3 * a_n_1 + 4 * b_n_1 + 3 * c_n_1, c_n, 2 * a_n_1 + 3 * b_n_1 + c_n_1
+        vector = np.array([a_n, b_n, c_n])
+        normed = ((a_n ** 2) + (b_n ** 2) + (c_n ** 2)) ** (1 / 2)
+        limit = vector / normed
+        q_n_2 = np.matmul(np.matmul(limit, A),limit)
+        x_values_2.append(x)
+        epsilon_values_2.append(abs(4-q_n_2))
+        if abs(4-q_n_2) < 10**(-8):
+            print(x)
+            break
+    for x in range(x+1, 500):
+        a_n_1, a_n, b_n_1, b_n, c_n_1, c_n = a_n, a_n_1 + 3 * b_n_1 + 2 * c_n_1, b_n, -3 * a_n_1 + 4 * b_n_1 + 3 * c_n_1, c_n, 2 * a_n_1 + 3 * b_n_1 + c_n_1
+        vector = np.array([a_n, b_n, c_n])
+        normed = ((a_n ** 2) + (b_n ** 2) + (c_n ** 2)) ** (1 / 2)
+        limit = vector / normed
+        q_n_2 = np.matmul(np.matmul(limit, A),limit)
+        x_values_2.append(x)
+        epsilon_values_2.append(abs(4 - q_n_2))
+        if abs(4 - q_n_2) < 10**(-16):
+            print(x)
+            break
 
 recursive()
+
+plot.xlabel('Iterates')
+plot.ylabel('Epsilon')
+plot.semilogy(x_values_2, epsilon_values_2)
+plot.show()
 
 def recursive_2():
     a_n_1, a_n, b_n_1, b_n, c_n_1, c_n = 8, 41, 3, 24, 12, 37
@@ -33,31 +64,25 @@ def recursive_2():
         vector = np.array([a_n, b_n, c_n])
         normed = ((a_n ** 2) + (b_n ** 2) + (c_n ** 2)) ** (1 / 2)
         limit_2 = vector / normed
-        epsilon = 10 ** (-8)
-        diff = np.linalg.norm(limit - limit_2)
         x_values.append(x)
-        epsilon_values.append(diff)
-        if diff < epsilon:
+        epsilon_values.append(np.linalg.norm(limit - limit_2))
+        if np.linalg.norm(limit - limit_2) < 10 ** (-8):
             print(x)
-            print(diff)
             break
     for x in range(x+1,500):
         a_n_1, a_n, b_n_1, b_n, c_n_1, c_n = a_n, a_n_1 + 3 * b_n_1 + 2 * c_n_1, b_n, -3 * a_n_1 + 4 * b_n_1 + 3 * c_n_1, c_n, 2 * a_n_1 + 3 * b_n_1 + c_n_1
         vector = np.array([a_n, b_n, c_n])
         normed = ((a_n ** 2) + (b_n ** 2) + (c_n ** 2)) ** (1 / 2)
         limit_2 = vector / normed
-        epsilon = 10 ** (-16)
-        diff = np.linalg.norm(limit - limit_2)
         x_values.append(x)
-        epsilon_values.append(diff)
-        if diff < epsilon:
+        epsilon_values.append(np.linalg.norm(limit - limit_2))
+        if np.linalg.norm(limit - limit_2) < 10 ** (-16):
             print(x)
-            print(diff)
             return None
 
 recursive_2()
 
 plot.xlabel('Iterates')
-plot.ylabel('ε')
+plot.ylabel('Epsilon')
 plot.semilogy(x_values, epsilon_values)
 plot.show()
