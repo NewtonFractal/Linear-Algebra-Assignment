f9a32636a4646dc1fbcbaac6fbb854407e8fc08f
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-10-10 15:14:37 +0200

    Update 2.1.2.py

--- 2.1.2.py
+++ 2.1.2.py
@@ -1,4 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plot
+
+x_values = []
+epsilon_values = []
 
 A = np.array([[1, 3, 2],
               [-3, 4, 3],
@@ -24,13 +28,15 @@ def recursive_2():
     limit = vector / normed
     print(limit)
     a_n_1, a_n, b_n_1, b_n, c_n_1, c_n = 8, 41, 3, 24, 12, 37
-    for x in range(0, 500):
+    for x in range(1, 500):
         a_n_1, a_n, b_n_1, b_n, c_n_1, c_n = a_n, a_n_1 + 3 * b_n_1 + 2 * c_n_1, b_n, -3 * a_n_1 + 4 * b_n_1 + 3 * c_n_1, c_n, 2 * a_n_1 + 3 * b_n_1 + c_n_1
         vector = np.array([a_n, b_n, c_n])
         normed = ((a_n ** 2) + (b_n ** 2) + (c_n ** 2)) ** (1 / 2)
         limit_2 = vector / normed
         epsilon = 10 ** (-8)
         diff = np.linalg.norm(limit - limit_2)
+        x_values.append(x)
+        epsilon_values.append(diff)
         if diff < epsilon:
             print(x)
             print(diff)
@@ -42,10 +48,16 @@ def recursive_2():
         limit_2 = vector / normed
         epsilon = 10 ** (-16)
         diff = np.linalg.norm(limit - limit_2)
+        x_values.append(x)
+        epsilon_values.append(diff)
         if diff < epsilon:
             print(x)
             print(diff)
             return None
 
-
 recursive_2()
+
+plot.xlabel('Iterates')
+plot.ylabel('ε')
+plot.semilogy(x_values, epsilon_values)
+plot.show()
