cb6b563eb4374d61ef8ef6a49da33a82f56aad99
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-10-19 16:46:29 +0200

    Update 2.1.py

--- 2.1.py
+++ 2.1.py
@@ -1,30 +1,22 @@
 import numpy as np
+import matplotlib.pyplot as plot
 
-A = np.array([[1, 3,  2],
+normed_values = []
+x_values = []
+
+A = np.array([[1, 3, 2],
               [-3, 4, 3],
-              [2, 3,  1]])
+              [2, 3, 1]])
 
 def recursive():
-    a_n_1,a_n, b_n_1,b_n, c_n_1,c_n = 8, 41, 3, 24, 12, 37
-    for x in range(0,57):
+    a_n_1, a_n, b_n_1, b_n, c_n_1, c_n = 8, 41, 3, 24, 12, 37
+    for x in range(0, 500):
         a_n_1, a_n, b_n_1, b_n, c_n_1, c_n = a_n, a_n_1 + 3 * b_n_1 + 2 * c_n_1, b_n, -3 * a_n_1 + 4 * b_n_1 + 3 * c_n_1, c_n, 2 * a_n_1 + 3 * b_n_1 + c_n_1
-    vector = [a_n,b_n,c_n]
-    limit = vector/np.linalg.norm(vector)
-    print(limit)
-    q_n = np.matmul(np.matmul(vector,A),vector)
-    print(q_n)
-    a_n_1,a_n, b_n_1,b_n, c_n_1,c_n = 8, 41, 3, 24, 12, 37
-    for x in range(0,57):
-        a_n_1, a_n, b_n_1, b_n, c_n_1, c_n = a_n, a_n_1 + 3 * b_n_1 + 2 * c_n_1, b_n, -3 * a_n_1 + 4 * b_n_1 + 3 * c_n_1, c_n, 2 * a_n_1 + 3 * b_n_1 + c_n_1
-        vector = [a_n, b_n, c_n]
-        limit_2 = vector/np.linalg.norm(vector)
-        epsilon = 10**(-8)
-        diff = np.linalg.norm(limit-limit_2)
-        print(diff)
-        if diff < epsilon:
-            print(x)
-            break
-
-
+        normed_values.append(((a_n ** 2) + (b_n ** 2) + (c_n ** 2)) ** (1 / 2)), x_values.append(x)
 
 recursive()
+
+plot.xlabel('Iterates')
+plot.ylabel('Normed Value')
+plot.semilogy(x_values, normed_values)
+plot.show()
