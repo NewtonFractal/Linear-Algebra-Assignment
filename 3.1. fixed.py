d40e20cda3dae667227397dd6ad7f2559a2607f0
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-10-21 18:41:31 +0200

    Update 3.1. fixed.py

--- 3.1. fixed.py
+++ 3.1. fixed.py
@@ -5,28 +5,29 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator
 import numpy as np
 
-def equation_solver(x_3):
-    x_1 = x_3[0]
-    x_2 = x_3[1]
-    zeroes = np.zeros(2)
-    zeroes[0] = (2*x_1-5*x_2+((27*(x_1**2)+2)**(1/2)))/2
-    zeroes[1] = (2*x_1-5*x_2-((27*(x_1**2)+2)**(1/2)))/2
-    return zeroes
+def equation_solver(x_1,x_2):
+    x_3_pos = (2*x_1-5*x_2+((27*(x_1**2)+2)**(1/2)))/2
+    x_3_neg = (2*x_1-5*x_2-((27*(x_1**2)+2)**(1/2)))/2
+    return [x_3_pos,x_3_neg]
 
 x = np.linspace(-1, 1, 300)
 y = np.linspace(-1, 1, 300)
 
 X, Y = np.meshgrid(x, y)
-Z = fsolve(equation_solver,[1.0,1.0])
+Z = equation_solver(X,Y)[0]
 
-Z = np.linalg.norm(Z)
-
-print(Z)
-
-print(Z.shape)
 
 fig = plt.figure()
 ax = plt.axes(projection="3d")
 ax.plot_wireframe(X, Y, Z, color='green')
 
 plt.show()
+
+Z = equation_solver(X,Y)[1]
+
+fig = plt.figure()
+ax = plt.axes(projection="3d")
+ax.plot_wireframe(X, Y, Z, color='green')
+
+plt.show()
+
