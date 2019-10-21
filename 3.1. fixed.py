7f908d8228548b4ac581e36f4c16e0cbfca44f11
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-10-21 17:58:16 +0200

    Update 3.1. fixed.py

--- 3.1. fixed.py
+++ 3.1. fixed.py
@@ -5,12 +5,25 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator
 import numpy as np
 
+def equation_solver(x_3):
+    x_1 = x_3[0]
+    x_2 = x_3[1]
+    zeroes = np.zeros(2)
+    zeroes[0] = (2*x_1-5*x_2+((27*(x_1**2)+2)**(1/2)))/2
+    zeroes[1] = (2*x_1-5*x_2-((27*(x_1**2)+2)**(1/2)))/2
+    return zeroes
+
 x = np.linspace(-1, 1, 300)
 y = np.linspace(-1, 1, 300)
 
 X, Y = np.meshgrid(x, y)
-Z = (2*Y-5*X+((27*(X**2)+2)**(1/2)))/2
+Z = fsolve(equation_solver,[1.0,1.0])
 
+Z = np.linalg.norm(Z)
+
+print(Z)
+
+print(Z.shape)
 
 fig = plt.figure()
 ax = plt.axes(projection="3d")
